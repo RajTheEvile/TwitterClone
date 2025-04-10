acc5b68134600282de00ccf21eef3671cd418a1b
RajTheEvileye <03.05.2004.r@gmail.com>
2025-04-10 14:12:10 +0530

    updating server dependency bycrpt to bycrptjs for render backend as it uses linux backed

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const sqlite3 = require("sqlite3");
 const { open } = require("sqlite");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const app = express();
 app.use(express.json());
